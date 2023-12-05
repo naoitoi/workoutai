c9f4a7bab6a59aa835c1f1ef52c9fc785c2fa25f
Naomaru Itoi <naomaru.itoi@gmail.com>
2023-12-04 23:40:23 -0800

    Calcurate stride ratios

---
+++ WorkoutUtils.py
@@ -0,0 +1,28 @@
+import cv2
+
+class WorkoutUtils:
+    # Dictionary that maps from joint names to keypoint indices.
+
+    KEYPOINT_DICT = {
+        'nose': 0,
+        'left_eye': 1,
+        'right_eye': 2,
+        'left_ear': 3,
+        'right_ear': 4,
+        'left_shoulder': 5,
+        'right_shoulder': 6,
+        'left_elbow': 7,
+        'right_elbow': 8,
+        'left_wrist': 9,
+        'right_wrist': 10,
+        'left_hip': 11,
+        'right_hip': 12,
+        'left_knee': 13,
+        'right_knee': 14,
+        'left_ankle': 15,
+        'right_ankle': 16
+    }
+
+    @staticmethod
+    def draw_line_between_keypoints(frame, start, end, color, thickness=3):
+        cv2.line(frame, start, end, color, thickness)
